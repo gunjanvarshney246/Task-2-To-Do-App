dcbed5ed02ec96889daf1cbf2beeb86209b2f029
gunjanvarshney246 <varshneygunjan25@gmail.com>
2026-02-18 21:18:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const input = document.getElementById("taskInput");
+const addBtn = document.getElementById("addBtn");
+const taskList = document.getElementById("taskList");
+
+addBtn.addEventListener("click", () => {
+  const taskText = input.value.trim();
+
+  if (taskText === "") return;
+
+  const li = document.createElement("li");
+  li.textContent = taskText;
+
+  li.addEventListener("click", () => {
+    li.classList.toggle("completed");
+  });
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "X";
+
+  deleteBtn.addEventListener("click", (e) => {
+    e.stopPropagation();
+    li.remove();
+  });
+
+  li.appendChild(deleteBtn);
+  taskList.appendChild(li);
+
+  input.value = "";
+});
